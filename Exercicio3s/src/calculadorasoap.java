009b9a9a98945ea5e5173f5a849e3ed641c52dbd
Bruno Soares dos santos <83316195+BrunoSoaresdosSantos@users.noreply.github.com>
2023-03-02 20:07:44 -0300

    Add files via upload

---
+++ Exercicio3s/src/calculadorasoap.java
@@ -0,0 +1,17 @@
+public class calculadorasoap implements Calculadora {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
